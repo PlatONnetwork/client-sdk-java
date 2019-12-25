9bb84b76302cfe3d70fe1a2ad2dcc07b3c3aebc5
lhdeng <15821747496@163.com>
2019-12-25 14:13:51 +0800

    Fix issue with event signature error.

--- abi/src/main/java/org/web3j/abi/datatypes/Event.java
+++ abi/src/main/java/org/web3j/abi/datatypes/Event.java
@@ -2,67 +2,46 @@ package org.web3j.abi.datatypes;
 
 import org.web3j.abi.TypeReference;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import static org.web3j.abi.Utils.convert;
 
 /**
  * Event wrapper type.
  */
+@SuppressWarnings("rawtypes")
 public class Event {
-    private String name;
-    private int functionType;
-    private List<TypeReference<Type>> indexedParameters = new ArrayList<TypeReference<Type>>();
-    ;
-    private List<TypeReference<Type>> nonIndexedParameters = new ArrayList<TypeReference<Type>>();
+	private String name;
+	private int functionType;
+	private List<TypeReference<Type>> parameters;
 
-    public Event(String name, List<TypeReference<?>> parameters) {
-        this.name = name;
-        this.nonIndexedParameters = convert(parameters);
-    }
+	public Event(String name, List<TypeReference<?>> parameters) {
+		this.name = name;
+		this.parameters = convert(parameters);
+	}
 
-    public Event(int functionType, List<TypeReference<?>> parameters) {
-        this.functionType = functionType;
-        this.nonIndexedParameters = convert(parameters);
-    }
+	public Event(int functionType, List<TypeReference<?>> parameters) {
+		this.functionType = functionType;
+		this.parameters = convert(parameters);
+	}
 
-    public Event(String name, List<TypeReference<?>> indexedParameters,
-                 List<TypeReference<?>> nonIndexedParameters) {
-        this.name = name;
-        this.indexedParameters = convert(indexedParameters);
-        this.nonIndexedParameters = convert(nonIndexedParameters);
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Event(int functionType, List<TypeReference<?>> indexedParameters,
-                 List<TypeReference<?>> nonIndexedParameters) {
-        this.functionType = functionType;
-        this.indexedParameters = convert(indexedParameters);
-        this.nonIndexedParameters = convert(nonIndexedParameters);
-    }
+	public int getFunctionType() {
+		return functionType;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public List<TypeReference<Type>> getParameters() {
+		return parameters;
+	}
 
-    public int getFunctionType() {
-        return functionType;
-    }
+	public List<TypeReference<Type>> getIndexedParameters() {
+		return parameters.stream().filter(TypeReference::isIndexed).collect(Collectors.toList());
+	}
 
-    public List<TypeReference<Type>> getIndexedParameters() {
-        return indexedParameters;
-    }
-
-    public List<TypeReference<Type>> getNonIndexedParameters() {
-        return nonIndexedParameters;
-    }
-
-    private List<TypeReference<Type>> convert(List<TypeReference<?>> parameters) {
-        List<TypeReference<Type>> typeReferenceList = new ArrayList<TypeReference<Type>>();
-        for (TypeReference<?> typeReference : parameters) {
-            if (typeReference.isIndexed()) {
-                continue;
-            }
-            typeReferenceList.add((TypeReference<Type>) typeReference);
-        }
-        return typeReferenceList;
-    }
+	public List<TypeReference<Type>> getNonIndexedParameters() {
+		return parameters.stream().filter(p -> !p.isIndexed()).collect(Collectors.toList());
+	}
 }
